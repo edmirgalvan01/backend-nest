8936f771a2eae98738c5810067692778c3449027
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-21 17:03:46 -0500

    Se crearon carpetas para los controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products/products.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
 
 @Module({
   imports: [],
