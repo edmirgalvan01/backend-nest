e327f072afa14f5bdd92de9313891b6324b7297c
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-21 23:12:45 -0500

    Se crearon las funciones del controlador de orders

--- src/controllers/orders/orders.controller.ts
+++ src/controllers/orders/orders.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  @Get('')
+  getOrders(@Query('limit') limit = 30, @Query('offset') offset: number) {
+    return `Orders: ${limit} - ${offset}`;
+  }
+
+  @Get(':id')
+  getOrder(@Param('id') id: any) {
+    return `Order id: ${id}`;
+  }
+}
