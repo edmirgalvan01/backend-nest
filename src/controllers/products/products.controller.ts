fc7ccdd3d666826ee2f4936d42e98510685de4ec
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-22 17:35:16 -0500

    Se creo el metodo POST de products

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post } from '@nestjs/common';
 
 //Any method will have this path (products), so it is not necessary to put it in the method
 @Controller('products')
 export class ProductsController {
   @Get('')
   //<functionName>(@Query(<paramName>) <paramName>: <typeOfParam>)
-  getProducts(
+  getAll(
     //we can define default params
     @Query('limit') limit = 100,
     @Query('offset') offset: number,
@@ -17,7 +17,15 @@ export class ProductsController {
   //This is the way to create a endpoint getting params from the url
   @Get(':id')
   //<functionName>(@Param(<paramName>) <paramName>: <typeOfParam>)
-  getProduct(@Param('id') id: any) {
+  getOne(@Param('id') id: any) {
     return `Product: ${id}`;
+  }
+
+  //Create a new product
+  @Post()
+  create() {
+    return {
+      message: 'Accion de crear',
+    };
   }
 }
