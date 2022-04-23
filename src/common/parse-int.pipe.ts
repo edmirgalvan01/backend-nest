88d90e545f0003c94193700c6a9204d2e0aae6e9
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-23 11:08:31 -0500

    Agregamos comentarios al pipe

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -3,10 +3,14 @@ import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
   transform(value: string) {
+    //We parse the value to number
     const val = parseInt(value, 10);
+
+    //If the value is Not a Number (NaN) we return an error
     if (isNaN(val)) {
       throw new BadRequestException(`${value} is not an number`);
     }
+
     return val;
   }
 }
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -2,7 +2,6 @@ import {
   Controller,
   Get,
   Param,
-  Query,
   Post,
   Body,
   Put,
