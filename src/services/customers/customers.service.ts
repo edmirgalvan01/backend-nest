d04b0c79e4520678797ca26a9e622753c0bc46f8
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-25 10:48:04 -0500

    Se crearon los servicios para customers

--- src/services/customers/customers.service.ts
+++ src/services/customers/customers.service.ts
@@ -1,4 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Customer } from '../../entities/customer.entity';
+import { CreateCustomerDto, UpdateCustomerDto } from '../../dtos/customer.dto';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  private counterId = 1;
+
+  private customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Edmir',
+      age: 19,
+      email: 'edmirgalvaz@gmail.com',
+    },
+  ];
+
+  findAll() {
+    return this.customers;
+  }
+
+  findOne(id: number) {
+    const customer = this.customers.find((c) => c.id === id);
+
+    if (!customer) {
+      throw new NotFoundException(`customer #${id} not found`);
+    } else {
+      return null;
+    }
+
+    return customer;
+  }
+
+  create(payload: CreateCustomerDto) {
+    this.counterId += 1;
+
+    const newCustomer = {
+      id: this.counterId,
+      ...payload,
+    };
+
+    this.customers.push(newCustomer);
+
+    return newCustomer;
+  }
+
+  update(id: number, payload: UpdateCustomerDto) {
+    const customer = this.findOne(id);
+    if (customer) {
+      const indexCustomer = this.customers.findIndex((c) => c.id === id);
+      this.customers[indexCustomer] = {
+        ...customer,
+        ...payload,
+      };
+
+      return this.customers[indexCustomer];
+    } else {
+      return null;
+    }
+  }
+
+  delete(id: number) {
+    const customer = this.findOne(id);
+    if (customer) {
+      const indexCustomer = this.customers.findIndex((c) => c.id === id);
+      this.customers.splice(indexCustomer, 1);
+
+      return { message: true };
+    } else {
+      return { message: false };
+    }
+  }
+}
