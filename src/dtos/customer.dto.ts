9a53cffa173541476f04d7d60468392816a8294a
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-25 10:35:48 -0500

    Se creo la entidad y el dto de customer

---
+++ src/dtos/customer.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
+}
+
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
