6865aac2fec38d3095e6bd23c1eda3ebd7eb0007
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-04-25 11:19:49 -0500

    Se crearon los servicios de orders

--- src/services/orders/orders.service.ts
+++ src/services/orders/orders.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Order } from '../../entities/order.entity';
+import { CreateOrderDto, UpdateOrderDto } from '../../dtos/order.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  private counterId = 1;
+
+  private orders: Order[] = [
+    {
+      id: 1,
+      customer: 'Edmir',
+      provider: 'Miguel',
+    },
+  ];
+
+  findAll() {
+    return this.orders;
+  }
+
+  findOne(id: number) {
+    const order = this.orders.find((o) => o.id === id);
+    if (!order) {
+      throw new NotFoundException(`Order #${id} not found`);
+    }
+    return order;
+  }
+
+  create(payload: CreateOrderDto) {
+    this.counterId = this.counterId + 1;
+    const newOrder = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.orders.push(newOrder);
+
+    return newOrder;
+  }
+
+  update(id: number, payload: UpdateOrderDto) {
+    const order = this.findOne(id);
+    if (order) {
+      const indexOrder = this.orders.findIndex((o) => o.id === id);
+      this.orders[indexOrder] = {
+        ...order,
+        ...payload,
+      };
+      return this.orders[indexOrder];
+    } else {
+      return null;
+    }
+  }
+
+  delete(id: number) {
+    const order = this.findOne(id);
+    if (order) {
+      const indexOrder = this.orders.findIndex((o) => o.id === id);
+      this.orders.splice(indexOrder, 1);
+      return { message: true };
+    } else {
+      return { message: false };
+    }
+  }
+}
